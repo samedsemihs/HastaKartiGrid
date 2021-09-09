dee671e0cc9402969c7174263a1fcbc87c3c7a0b
Samed Semih <samedsemihs@gmail.com>
2021-09-10 00:26:22 +0300

    Add files via upload
    bitti sayılır

--- src/KartView.java
+++ src/KartView.java
@@ -3,38 +3,37 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.lang.reflect.Field;
 
-public class KartView extends JFrame implements ActionListener {
-
-    private int genislik = 910;
-    private int yukseklik = 700;
+public class KartView extends JFrame implements ActionListener, MouseListener {
 
     JPanel ustPanel, altPanel;
     JSplitPane divPanel;
     JScrollPane jScrollPane;
-
     JTable table;
     DefaultTableModel tableModel;
     ImageIcon imgProfilResmi = new ImageIcon(new ImageIcon("./kaynaklar/resimler/pp_placeholder.png").getImage().getScaledInstance(181, 147, Image.SCALE_DEFAULT));
-
     JTextField txtTC, txtAdSoyad;
     JTextField txtTelefon, txtAdres;
     JTextField txtMedeni, txtKan;
     JTextField txtCinsiyet, txtDogumTarihi;
-
     JLabel lblTC, lblAdSoyad;
     JLabel lblTelefon, lblAdres;
     JLabel lblMedeni, lblKan, lblResimCerceve;
     JLabel lblCinsiyet, lblDogumTarihi;
-
     JButton btnKaydet, btnSil, btnTemizle, btnFotoSec;
-
     Dimension boyut = Toolkit.getDefaultToolkit().getScreenSize();
+    //Timer sayac;
+    private final int genislik = 910;
+    private final int yukseklik = 700;
 
     KartView() {
         //frame ayarları
         this.setSize(genislik, yukseklik);
-        this.setLocation(boyut.width / 2 - this.getSize().width / 2 , boyut.height / 2 - this.getSize().height / 2);//ortala
+        this.setLocation(boyut.width / 2 - this.getSize().width / 2, boyut.height / 2 - this.getSize().height / 2);//ortala
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setTitle("ERÜ Tıp Hasta Kartı");
         this.setResizable(false);
@@ -106,6 +105,8 @@ public class KartView extends JFrame implements ActionListener {
         btnFotoSec.setToolTipText("Profil resminizi değiştirmek için tıklayın.");
         btnFotoSec.addActionListener(this);
 
+        //sayac = new Timer(400, this);
+
         String[][] veri = {
                 {"12345678911", "Deneme", "5558833554", "deneme adresi", "05.08.1994", "B+", "Erkek", "Evli", "path/image.png"},
                 {"21312213213", "Deneme2", "5558833555", "deneme adresi2", "05.08.1990", "A+", "Kadın", "Bekar", "path/image.jpg"}
@@ -115,24 +116,25 @@ public class KartView extends JFrame implements ActionListener {
 
         tableModel = new DefaultTableModel(veri, sutun);
         table = new JTable(tableModel);
+        table.addMouseListener(this);
         jScrollPane = new JScrollPane();
-        jScrollPane.setBounds(19,0,870,300);
+        jScrollPane.setBounds(19, 0, 870, 300);
         jScrollPane.add(table);
         jScrollPane.setViewportView(table);//tablonun görünmesi için
         table.setFillsViewportHeight(true);//tablonun tüm alanı kaplaması için
 
         btnKaydet = new JButton("Kaydet");
-        btnKaydet.setBounds(400,320,150,25);
+        btnKaydet.setBounds(400, 320, 150, 25);
         btnKaydet.setToolTipText("Hasta kaydetmek için tıklayın.");
         btnKaydet.addActionListener(this);//btn clk için
 
         btnTemizle = new JButton("Temizle");
-        btnTemizle.setBounds(560,320,150,25);
+        btnTemizle.setBounds(560, 320, 150, 25);
         btnTemizle.setToolTipText("Kayıt alanlarını temizlemek için tıklayın.");
         btnTemizle.addActionListener(this);//btn clk için
 
         btnSil = new JButton("Hasta Kyt. Sil");
-        btnSil.setBounds(720,320,150,25);
+        btnSil.setBounds(720, 320, 150, 25);
         btnSil.setToolTipText("Hasta kaydını silmek için tıklayın.");
         btnSil.addActionListener(this);//btn clk için
 
@@ -188,23 +190,115 @@ public class KartView extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnKaydet){
-            hastaEkle(txtTC.getText(), txtAdSoyad.getText(), txtTelefon.getText(), txtAdres.getText(), txtDogumTarihi.getText(),
-                        txtKan.getText(), txtCinsiyet.getText(), txtMedeni.getText(), imgProfilResmi.);
-        }
-    }
+        String tc, adSoyad, telefon, adres, kan, cinsiyet, medeni, dogumTar, imgAdres = "";
+        imgAdres =
+
+        tc = txtTC.getText();
+        adSoyad = txtAdSoyad.getText();
+        telefon = txtTelefon.getText();
+        adres = txtAdres.getText();
+        kan = txtKan.getText();
+        cinsiyet = txtCinsiyet.getText();
+        medeni = txtMedeni.getText();
+        dogumTar = txtDogumTarihi.getText();
 
 
-    static void hastaEkle(String tc, String adSoyad, String telefon, String adres, String dogumTarihi, String kan, String cinsiyet, String medeniHal, String profilResmiPath) {
-        if (anahtar.getText().equals("") || karsilik.getText().equals("")) {
-            anahtar.requestFocus();
-        } else {
-            for (int i = 0; i < tm.getRowCount(); i++) {
-                if (tm.getValueAt(i, 0).toString().equals(tc)) {
+        if (e.getSource() == btnKaydet) {
+            Field field = null;
+            try {
+                field = imgProfilResmi.getClass().getField("filename");
+            } catch (NoSuchFieldException ex) {
+                ex.printStackTrace();
+            }
+            field.setAccessible(true);
+
+            try {
+                imgAdres = (String) field.get(imgProfilResmi);
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+
+            if (txtTC.getText().equals("")) txtTC.requestFocus();
+
+            for (int i = 0; i < tableModel.getRowCount(); i++) {
+                if (tableModel.getValueAt(i, 0).toString().equals(txtTC.getText())) {
                     return;
                 }
             }
-            tm.insertRow(table.getRowCount(), new Object[]{key, pair});
+            tableModel.insertRow(table.getRowCount(), new Object[]{tc, adSoyad, telefon, adres, dogumTar, kan, cinsiyet, dogumTar, imgAdres});
         }
+
+        if (e.getSource() == btnSil) {
+            int[] satirlar = table.getSelectedRows();
+
+            for (int i = 0; i < satirlar.length; i++) {
+                if (satirlar[i] > 0)
+                    tableModel.removeRow(satirlar[i] - i);
+                else
+                    return;
+            }
+        }
+
+        if (e.getSource() == btnTemizle) {
+            txtTC.setText("");
+            txtAdres.setText("");
+            txtTelefon.setText("");
+            txtCinsiyet.setText("");
+            txtKan.setText("");
+            txtDogumTarihi.setText("");
+            txtMedeni.setText("");
+            txtAdSoyad.setText("");
+            imgProfilResmi = new ImageIcon(new ImageIcon("./kaynaklar/resimler/pp_placeholder.png").getImage().getScaledInstance(181, 147, Image.SCALE_DEFAULT));
+            lblResimCerceve.setIcon(imgProfilResmi);
+        }
+
+        if (e.getSource() == btnFotoSec) {
+            FileDialog fd = new FileDialog(new JFrame());
+            fd.setVisible(true);
+            File[] f = fd.getFiles();
+            String imgYol = fd.getFiles()[0].getAbsolutePath();
+            if (f.length > 0) {
+                imgProfilResmi = new ImageIcon(new ImageIcon(imgYol).getImage().getScaledInstance(181, 147, Image.SCALE_DEFAULT));
+                lblResimCerceve.setIcon(imgProfilResmi);
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == table) {
+            int[] satirlar = table.getSelectedRows();
+
+            for (int i = 0; i < satirlar.length; i++) {
+                txtTC.setText(tableModel.getValueAt(satirlar[i], 0).toString());
+                txtAdSoyad.setText(tableModel.getValueAt(satirlar[i], 1).toString());
+                txtTelefon.setText(tableModel.getValueAt(satirlar[i], 2).toString());
+                txtAdres.setText(tableModel.getValueAt(satirlar[i], 3).toString());
+                txtDogumTarihi.setText(tableModel.getValueAt(satirlar[i], 4).toString());
+                txtKan.setText(tableModel.getValueAt(satirlar[i], 5).toString());
+                txtCinsiyet.setText(tableModel.getValueAt(satirlar[i], 6).toString());
+                txtMedeni.setText(tableModel.getValueAt(satirlar[i], 7).toString());
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
